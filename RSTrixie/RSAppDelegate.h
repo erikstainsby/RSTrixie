7d5c7bf750f615155690a19f8ed40a3c098239eb
Erik Stainsby <erik@roaringsky.local>
2012-02-15 20:29:41 -0800

    Initial Commit

---
+++ RSTrixie/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  RSTrixie
+//
+//  Created by Erik Stainsby on 12-02-15.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ RSTrixie/RSAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  RSAppDelegate.m
+//  RSTrixie
+//
+//  Created by Erik Stainsby on 12-02-15.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "RSAppDelegate.h"
+
+@implementation RSAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
