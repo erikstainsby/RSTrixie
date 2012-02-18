c92639979a33efda41595daa1a1a55fe556b4af6
Erik Stainsby <erik@roaringsky.local>
2012-02-18 05:52:59 -0800

    Pre-browser refactor

--- RSTrixie/RSAppDelegate.h
+++ RSTrixie/RSAppDelegate.h
@@ -8,14 +8,33 @@
 
 #import <Cocoa/Cocoa.h>
 #import <RSTrixiePlugin/RSTrixiePlugin.h>
+#import "NSView+RSPositionView.h"
 
 @interface RSAppDelegate : NSObject <NSApplicationDelegate>
 
-@property (retain) IBOutlet NSBox * box;
-@property (retain) IBOutlet NSArray * plugins;
+@property (retain) IBOutlet NSWindowController * windowController;
 @property (assign) IBOutlet NSWindow * window;
-@property (retain) IBOutlet NSPopUpButton * pluginMenu;
+
+@property (retain) IBOutlet NSSegmentedControl * segmentedControl;
+
+@property (retain) IBOutlet NSBox * box1;
+@property (retain) IBOutlet NSBox * box2;
+@property (retain) IBOutlet NSBox * box3;
+
+@property (retain) IBOutlet NSView * actionPanel;
+@property (retain) IBOutlet NSView * reactionPanel;
+@property (retain) IBOutlet NSView * conditionPanel;
+@property (retain) IBOutlet NSView * commentPanel;
+
+@property (retain) IBOutlet NSArray * actionPlugins;
+@property (retain) IBOutlet NSArray * reactionPlugins;
+@property (retain) IBOutlet NSArray * conditionPlugins;
+
+@property (retain) IBOutlet NSPopUpButton * actionMenu;
+@property (retain) IBOutlet NSPopUpButton * reactionMenu;
+@property (retain) IBOutlet NSPopUpButton * conditionMenu;
 
 - (NSArray*) loadPlugins;
+- (NSArray*) loadPluginsWithPrefix:(NSString*)prefix;
 
 @end
--- RSTrixie/RSAppDelegate.m
+++ RSTrixie/RSAppDelegate.m
@@ -10,18 +10,46 @@
 
 @implementation RSAppDelegate
 
-@synthesize box;
+@synthesize windowController;
 @synthesize window = _window;
-@synthesize plugins;	// view controllers 
-@synthesize pluginMenu;
 
-- (id)init
-{
+@synthesize segmentedControl;
+
+@synthesize box1;
+@synthesize box2;
+@synthesize box3;
+
+@synthesize actionPanel;		// custom view
+@synthesize reactionPanel;		// custom view
+@synthesize conditionPanel;		// custom view
+@synthesize commentPanel;
+
+@synthesize actionPlugins;		// view controllers 
+@synthesize reactionPlugins;	// view controllers 
+@synthesize conditionPlugins;	// view controllers 
+
+@synthesize actionMenu;			// popup button
+@synthesize reactionMenu;		// popup button
+@synthesize conditionMenu;		// popup button
+
+
+static NSView * activeActionPlugin;
+static NSView * activeReactionPlugin;
+static NSView * activeConditionPlugin;
+
+- (id)init {
 	if(nil!=(self=[super init]))
 	{
-		[self setPlugins:[self loadPlugins]];
-		NSLog(@"%s- [%04d] %lu", __PRETTY_FUNCTION__, __LINE__, [plugins count]);
+		activeActionPlugin = box1;
+		activeReactionPlugin = box2;
+		activeConditionPlugin = box3;
 		
+		[self setActionPlugins:[self loadPluginsWithPrefix:@"Action"]];
+		[self setReactionPlugins:[self loadPluginsWithPrefix:@"Reaction"]];
+		[self setConditionPlugins:[self loadPluginsWithPrefix:@"Condition"]];
+		
+		windowController = [[NSWindowController alloc] initWithWindowNibName:@"RSTrixieEditor" owner:self];
+		[[windowController window] makeKeyAndOrderFront:self];
 	}
 	return self;
 }
@@ -29,15 +57,36 @@
 - (void) applicationWillFinishLaunching:(NSNotification *)notification {
 
 	NSMenu * menu = [[NSMenu alloc] init];	
-	
-	for(RSTrixiePlugin * p in plugins)
+	for(RSTrixiePlugin * p in actionPlugins)
 	{
-		NSMenuItem * menuItem = [[NSMenuItem alloc] initWithTitle:[p name] action:@selector(showPluginInView:) keyEquivalent:@""];
+		NSMenuItem * menuItem = [[NSMenuItem alloc] initWithTitle:[p name] action:@selector(showActionPlugin:) keyEquivalent:@""];
 		[menuItem setRepresentedObject:p];
 		[menu addItem:menuItem];
-		NSLog(@"%s- [%04d] added menu item for plugin: %@", __PRETTY_FUNCTION__, __LINE__, [p name]);
+		NSLog(@"%s- [%04d] added action menu item for plugin: %@", __PRETTY_FUNCTION__, __LINE__, [p name]);
 	}
-	[pluginMenu setMenu:menu];
+	[actionMenu setMenu:menu];
+	
+	
+	NSMenu * menu2 = [[NSMenu alloc] init];	
+	for(RSTrixiePlugin * p in reactionPlugins)
+	{
+		NSMenuItem * menuItem = [[NSMenuItem alloc] initWithTitle:[p name] action:@selector(showReactionPlugin:) keyEquivalent:@""];
+		[menuItem setRepresentedObject:p];
+		[menu2 addItem:menuItem];
+		NSLog(@"%s- [%04d] added reaction menu item for plugin: %@", __PRETTY_FUNCTION__, __LINE__, [p name]);
+	}
+	[reactionMenu setMenu:menu2];
+	
+	
+	NSMenu * menu3 = [[NSMenu alloc] init];	
+	for(RSTrixiePlugin * p in conditionPlugins)
+	{
+		NSMenuItem * menuItem = [[NSMenuItem alloc] initWithTitle:[p name] action:@selector(showConditionPlugin:) keyEquivalent:@""];
+		[menuItem setRepresentedObject:p];
+		[menu3 addItem:menuItem];
+		NSLog(@"%s- [%04d] added condition menu item for plugin: %@", __PRETTY_FUNCTION__, __LINE__, [p name]);
+	}
+	[conditionMenu setMenu:menu3];
 
 }
 
@@ -45,20 +94,108 @@
 	
 }
 
-static NSView * activePluginView = nil;
-
-- (IBAction) showPluginInView:(id)sender {
+- (IBAction) showActionPlugin:(id)sender {
+	
 	NSString * name = [sender title];
 	RSTrixiePlugin * p  = [sender representedObject];
-	if( activePluginView == nil) {
-		[box addSubview:[p view]];
+	
+	if( activeActionPlugin == nil) {
+		[actionPanel addSubview:[p view]];
 	}
-	else {
-		[box replaceSubview:activePluginView with:[p view]];
+	else {	
+		[actionPanel replaceSubview:activeActionPlugin with:[p view]];
 	}
-	activePluginView = [p view];
+	[[p view] setFrameTopLeftPoint:actionMenu.frame.origin];
+	
+	activeActionPlugin = [p view];
+	
 	NSLog(@"%s- [%04d] %@", __PRETTY_FUNCTION__, __LINE__, name);
 }
+
+
+- (IBAction) showReactionPlugin:(id)sender {
+	
+	NSString * name = [sender title];
+	RSTrixiePlugin * p  = [sender representedObject];
+	
+	if( activeReactionPlugin == nil) {
+		[reactionPanel addSubview:[p view]];
+	}
+	else {
+		[reactionPanel replaceSubview:activeReactionPlugin with:[p view]];
+	}
+	[[p view] setFrameTopLeftPoint:reactionMenu.frame.origin];
+			
+	activeReactionPlugin = [p view];
+	
+	NSLog(@"%s- [%04d] %@", __PRETTY_FUNCTION__, __LINE__, name);
+}
+
+
+- (IBAction) showConditionPlugin:(id)sender {
+	
+	NSString * name = [sender title];
+	RSTrixiePlugin * p  = [sender representedObject];
+	
+	if( activeConditionPlugin == nil) {
+		[conditionPanel addSubview:[p view]];
+	}
+	else {
+		[conditionPanel replaceSubview:activeConditionPlugin with:[p view]];
+	}
+	[[p view] setFrameTopLeftPoint:conditionMenu.frame.origin];	
+	
+	activeConditionPlugin = [p view];
+	
+	NSLog(@"%s- [%04d] %@", __PRETTY_FUNCTION__, __LINE__, name);
+}
+
+
+
+- (NSArray*) loadPluginsWithPrefix:(NSString*)prefix {
+	
+	NSBundle * main = [NSBundle mainBundle];
+	NSArray * all = [main pathsForResourcesOfType:@"bundle" inDirectory:@"../Plugins"];
+	
+	NSMutableArray * ourPlugins = [NSMutableArray array];
+	
+	id plugin = nil;
+	NSBundle * pluginBundle = nil;
+	
+	for(NSString * path in all)
+	{
+		NSString * filename = [path lastPathComponent];
+		
+		if( ![filename hasPrefix:prefix] ) 
+		{
+			continue;
+		}
+		
+		pluginBundle = [NSBundle bundleWithPath:path];
+		
+		id bundleId = [pluginBundle bundleIdentifier];
+		
+		NSLog(@"%s- [%04d] %@", __PRETTY_FUNCTION__, __LINE__, bundleId);
+		
+		[pluginBundle load];
+		
+		Class prinClass = [pluginBundle principalClass];
+		if(![prinClass isSubclassOfClass:[RSTrixiePlugin class]]) {
+				// skip 
+			continue;
+		}
+		
+		plugin = [[prinClass alloc] initWithNibName:[prinClass className] bundle:bundleId];
+		[ourPlugins addObject:plugin];
+		
+		plugin = nil;
+		pluginBundle = nil;
+	}
+	
+	return ourPlugins;
+}
+
+
 
 - (NSArray*) loadPlugins {
 	
